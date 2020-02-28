edd27525bbc18d0ff2cef5f212879fd99500ac39
Julian Losada <Julian_Losada@epam.com>
2020-02-28 16:19:43 +0100

    experimental driver

--- src/test/java/ExperimentalTests.java
+++ src/test/java/ExperimentalTests.java
@@ -1,12 +1,31 @@
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ExperimentalTests {
 
     @Test
-    public void downloadPDFTest(){
+    public void downloadPDFTest() throws InterruptedException {
 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Julian_Losada\\IdeaProjects\\serenitybddwebinspanishtraining\\src\\test\\resources\\webdriver\\windows\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        // Launch Website
+        driver.navigate().to("https://file-examples.com/wp-content/uploads/2017/10/file-sample_150kB.pdf");
+
+        Thread.sleep(5000);
+
+        driver.quit();
 
     }
+
+
 
 }
