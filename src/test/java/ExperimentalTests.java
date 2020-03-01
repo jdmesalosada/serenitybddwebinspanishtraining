2b2a60c0f5b520ff1523ccbc280c0306a9c859fd
Julian <jdmesalosada@gmail.com>
2020-03-01 08:31:01 +0100

    added pdf redade

--- src/test/java/ExperimentalTests.java
+++ src/test/java/ExperimentalTests.java
@@ -1,21 +1,40 @@
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ExperimentalTests {
 
     @Test
     public void downloadPDFTest() throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Julian_Losada\\IdeaProjects\\serenitybddwebinspanishtraining\\src\\test\\resources\\webdriver\\windows\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        File folder = new File(UUID.randomUUID().toString());
+        folder.mkdir();
+
+        Map<String, Object> chromePrefs = new HashMap<>();
+        //chromePrefs.put("download.default_directory", "/Users/julianmesa/Downloads");
+        chromePrefs.put("download.default_directory", folder.getAbsolutePath());
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.open_pdf_in_system_reader", false);
+        chromePrefs.put("plugins.always_open_pdf_externally", true);
+
+
+        chromePrefs.put("pdfjs.disabled", true);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Julian_Losada\\IdeaProjects\\serenitybddwebinspanishtraining\\src\\test\\resources\\webdriver\\windows\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/julianmesa/Documents/projects/serenitybddwebinspanishtraining/src/test/resources/webdriver/mac/chromedriver");
+        WebDriver driver = new ChromeDriver(options);
+
 
         // Launch Website
         driver.navigate().to("https://file-examples.com/wp-content/uploads/2017/10/file-sample_150kB.pdf");
@@ -25,7 +44,5 @@ public class ExperimentalTests {
         driver.quit();
 
     }
-
-
 
 }
--- src/test/java/starter/stepdefinitions/DownloadStatementsStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/DownloadStatementsStepDefinitions.java
@@ -3,41 +3,63 @@ package starter.stepdefinitions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.util.EnvironmentVariables;
+import starter.abilities.ReadPdf;
 import starter.navigation.NavigateTo;
+import starter.ui.search.SearchFor;
 
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import java.io.IOException;
+
+import static net.serenitybdd.screenplay.actors.OnStage.*;
 
 public class DownloadStatementsStepDefinitions {
 
     String name;
 
+    //Serenity properties - Serenity will instantiate this field automatically
+    private EnvironmentVariables environmentVariables;
+
+    String downloadFolder;
+    String fileName;
+
     @Given("(.*) has made purchases with his credit card")
     public void pepito_has_made_purchases_with_his_credit_card(String name) throws InterruptedException {
         this.name = name;
 
-        theActorCalled(name).attemptsTo(
-                NavigateTo.theDuckDuckGoHomePage());
+        downloadFolder = environmentVariables.optionalProperty("download.filesFolder")
+                .orElseThrow(IllegalArgumentException::new);
 
-        Thread.sleep(20000);
-
+        fileName = environmentVariables.optionalProperty("statements.fileName")
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     @Given("he already downloaded his statements")
     public void he_already_downloaded_his_statements() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        OnStage.setTheStage(new OnlineCast());
+
+        Actor actor = theActorCalled(name);
+
+        actor.attemptsTo(
+                NavigateTo.theDuckDuckGoHomePage()
+        );
+
     }
 
     @When("he reviews his transactions")
-    public void he_reviews_his_transactions() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void he_reviews_his_transactions() throws IOException {
+
+        String statementFilePath = String.format("%s/%s", downloadFolder, fileName);
+
+        theActorInTheSpotlight().whoCan(ReadPdf.downloadedInPath(statementFilePath));
+        String pdfText = ReadPdf.as(theActorInTheSpotlight()).getText();
     }
 
     @Then("he should see the transactions for last month")
     public void he_should_see_the_transactions_for_last_month() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
     }
 
 }
